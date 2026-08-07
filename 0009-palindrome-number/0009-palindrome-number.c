f256db1f65d263c346ed4872f444413b6cdff890
Prathyush P Rao <raoprathyush7@gmail.com>
2026-08-07 19:04:23 +0530

    Time: 0 ms (100%), Space: 8.8 MB (90.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+     if (x < 0)
+        return false;
+
+    int real = x;
+    long long rev = 0;
+
+    while (x > 0) {
+        int digit = x % 10;
+        rev = rev * 10 + digit;
+        x /= 10;
+    }
+
+    return rev == real;
+}
