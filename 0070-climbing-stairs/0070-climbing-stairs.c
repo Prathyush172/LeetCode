a5def4bcb5e0087da71a06bbbe4b52f5b6e27d09
Prathyush P Rao <raoprathyush7@gmail.com>
2026-08-19 16:28:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (76.89%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,20 @@
+int solve(int n, int dp[]) {
+    if (n <= 2)
+        return n;
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+
+    return dp[n];
+}
+
+int climbStairs(int n) {
+    int dp[n + 1];
+
+    for (int i = 0; i <= n; i++)
+        dp[i] = -1;
+
+    return solve(n, dp);
+}
