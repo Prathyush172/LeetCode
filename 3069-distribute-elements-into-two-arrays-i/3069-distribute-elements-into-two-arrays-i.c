9126137dcefc944b1fa33f7966ed3d21f20857f4
Prathyush P Rao <raoprathyush7@gmail.com>
2026-08-20 09:46:26 +0530

    Time: 0 ms (100%), Space: 12.3 MB (44%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.c
@@ -0,0 +1,31 @@
+int* resultArray(int* nums, int numsSize, int* returnSize) {
+    int* ans = malloc(numsSize * sizeof(int));
+
+    int arr1[numsSize];
+    int arr2[numsSize];
+
+    int n1 = 0;
+    int n2 = 0;
+
+    arr1[n1++] = nums[0];
+    arr2[n2++] = nums[1];
+
+    for (int i = 2; i < numsSize; i++) {
+        if (arr1[n1 - 1] > arr2[n2 - 1])
+            arr1[n1++] = nums[i];
+        else
+            arr2[n2++] = nums[i];
+    }
+
+    int k = 0;
+
+    for (int i = 0; i < n1; i++)
+        ans[k++] = arr1[i];
+
+    for (int i = 0; i < n2; i++)
+        ans[k++] = arr2[i];
+
+    *returnSize = numsSize;
+
+    return ans;
+}
