777a0640a6bb0bb19a176dd3cae8c5ef208994d4
Prathyush P Rao <raoprathyush7@gmail.com>
2026-09-11 10:20:45 +0530

    Time: 0 ms (100%), Space: 8.9 MB (87.5%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.c
@@ -0,0 +1,10 @@
+int scoreOfString(char* s) {
+    int score = 0;
+    int len = strlen(s);
+    
+    for (int i = 1; i < len; i++) {
+        score += abs(s[i] - s[i - 1]);
+    }
+    
+    return score;
+}
